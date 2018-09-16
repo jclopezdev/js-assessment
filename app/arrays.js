dea298f5a244117ea25af6d301994fdc8ef5193e
Juan Carlos López <jcarloslopezl@hotmail.com>
2018-09-16 18:15:23 -0500

    Fix arrays examples

--- app/arrays.js
+++ app/arrays.js
@@ -2,19 +2,39 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.arraysAnswers = {
   indexOf: function(arr, item) {
+    
+    for(var i = 0; i < arr.length; i++) {
+      if(arr[i] === item) {
+        return i;
+      }
+    }
+
+    return -1;
 
   },
 
   sum: function(arr) {
+    var sum = 0;
+    for(i = 0; i < arr.length; i++) {
+      sum += arr[i];
+    }
 
+    return sum;
   },
 
   remove: function(arr, item) {
+    for(i = 0; i < arr.length; i++){
+      if(arr[i] == item) {
+        arr.splice(i, 1);
+        i--;  // Prevent skipping an item
+      }
+    }
 
+    return arr;
   },
 
   removeWithoutCopy: function(arr, item) {
-
+    return array;
   },
 
   append: function(arr, item) {
